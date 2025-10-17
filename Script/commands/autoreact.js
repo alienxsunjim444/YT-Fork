d736109dd6f4e54e0a8c5cab0dea3b4aa28d5eb3
Araf 69 <alienxsunjim444@gmail.com>
2025-10-18 00:23:58 +0600

    Update autoreact.js

--- Script/commands/autoreact.js
+++ Script/commands/autoreact.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "autoreact",
   version: "1.1.1",
   hasPermission: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "Araf 69",
   description: "Bot React",
   commandCategory: "No Prefix",
   usages: '[]',
@@ -25,7 +25,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         body: ""
       }
       api.sendMessage(lab, threadID, messageID);
-    api.setMessageReaction("❤️", event.messageID, (err) => {}, true)
+    api.setMessageReaction("💦", event.messageID, (err) => {}, true)
           };
     if(react.includes("sakit") || react.includes("Sakit") || react.includes("saket") || react.includes("Saket") || react.includes("peyn") || react.includes("Peyn") || react.includes("Pain") || react.includes("mamatay") || react.includes("Mamatay") || react.includes("ayaw ko na") || react.includes("Ayaw ko na") || react.includes("saktan") || react.includes("Saktan") || react.includes("Sasaktan") || react.includes("sasaktan") || react.includes("sad") || react.includes("Sad") || react.includes("malungkot") || react.includes("Malungkot") || react.includes(" 😥") || react.includes("😰") || react.includes("😨") || react.includes("😢") || react.includes(":(") || react.includes("😔") || react.includes("😞") || react.includes("depress") || react.includes("stress") || react.includes("Stress") || react.includes("Depress") || react.includes("depression") || react.includes("Depression") || react.includes("kalungkutan") || react.includes("Kalungkutan") || react.includes("😭")) {
       var sad = {
@@ -39,21 +39,21 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         body: ""
       }
       api.sendMessage(flag, threadID, messageID);
-    api.setMessageReaction("🇧🇩", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🍷", event.messageID, (err) => {}, true)
           };
   if(react.includes("eve") || react.includes("Eve") || react.includes("morning") || react.includes("Morning") || react.includes("afternoon") || react.includes("Afternoon") || react.includes("evening") || react.includes("eat") || react.includes("Eat") || react.includes("night") || react.includes("nyt") || react.includes("Night") || react.includes("Nyt")) {
       var heart = {
         body: ""
       }
       api.sendMessage(heart, threadID, messageID);
-    api.setMessageReaction("❤", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🪶", event.messageID, (err) => {}, true)
             };
   if(react.includes("wow") || react.includes("robot")) {
       var heart = {
         body: ""
       }
       api.sendMessage(heart, threadID, messageID);
-    api.setMessageReaction("😮", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😪", event.messageID, (err) => {}, true)
             }
         }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
